b3182c85db89398a9d123cd9b48585989adc8d20
okikechinonso <chinonsookike@gmail.com>
2022-09-16 12:14:17 +0100

    add new fields

--- chatcrib/src/sign-up/sign-up.js
+++ chatcrib/src/sign-up/sign-up.js
@@ -1,28 +1,62 @@
-import React from 'react';
-import './sign-up.css';
+import React from "react";
+import "./sign-up.css";
 const SignUp = () => (
-    <div className='container'>
-        <div className='row'>
-            <div className='col-9'>
-                <h1>Don't have an Account yet?</h1>
-                <h3>Sign up here</h3>
-                <form>
-                    <div className="mb-3 row">
-                        <label for="exampleFormControlInput1" class="form-label">Full Name</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Full Name" />
-                    </div>
-                    <div className="mb-3 row">
-                        <label for="exampleFormControlInput1" class="form-label"></label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Full Name" />
-                    </div>
-                    <div className="mb-3 row">
-                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                    </div>
-                </form>
-            </div>
-        </div>
-
+  <div className="container">
+    <div className="row">
+      <div className="col-9">
+        <h1>Don't have an Account yet?</h1>
+        <h3>Sign up here</h3>
+        <form>
+          <div className="mb-3 row">
+            <label for="exampleFormControlInput1" class="form-label flex">
+              Full Name
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleFormControlInput1"
+              placeholder="Full Name"
+            />
+          </div>
+          <div className="mb-3 row">
+            <label for="exampleFormControlInput1" class="form-label flex">
+              Email address
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleFormControlInput1"
+              placeholder="name@example.com"
+            />
+          </div>
+          <div className="mb-3 row">
+            <label for="exampleFormControlInput1" class="form-label flex">
+              Password
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleFormControlInput1"
+              placeholder="Password"
+            />
+          </div>
+          <div className="mb-3 row">
+            <label for="exampleFormControlInput1" class="form-label flex">
+              User Type
+            </label>
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <button type="button" class="btn btn-primary flex">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
-)
+  </div>
+);
 export default SignUp;
