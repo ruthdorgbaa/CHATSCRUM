427e3a2a03059bbcf033229fd3051ea961a6219a
okikechinonso <chinonsookike@gmail.com>
2022-09-19 10:38:08 +0100

    new commit

--- chatcrib/src/components/scrumboard/scrumboard.jsx
+++ chatcrib/src/components/scrumboard/scrumboard.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { details } from '../../static/login-detail'
 import { Link } from 'react-router-dom';
+import './scrumboard.css'
 
 
 export class Scrumboard extends Component {
@@ -8,17 +9,29 @@ export class Scrumboard extends Component {
   render() {
     return (
       <div>
-         <nav className='nav'>
+         <nav className='nav bg-primary'>
           <h3>CHATSCRUM</h3>
           <div >
               <p>User Type: {details.userType}</p>
               <p>Project Name: {details.projectName}</p>
           </div>
       </nav>
-      <div className='welcome'>
-        <h2>Hello {details.fullname} Welcome to your scrumboard</h2>
-        <Link to='/'>Home</Link>
+      <h4>Hello {details.fullname} Welcome to your scrumboard</h4>
+      <div>
+        <div style={{display: "flex"}}>
+        <div className='task-container'>
+          <h3 className='bg-primary'>Weekly Tasks</h3>
         </div>
+        <div>
+        <div className='task-container'>
+          <h3 className='bg-primary'>Daily Target</h3>
+        </div>
+        </div>
+        
+        </div>
+        
+        <button class='btn btn-primary'>ADD TASK</button>
+      </div>
       </div>
     )
   }
