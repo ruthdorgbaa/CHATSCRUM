348b182f7f70ec2efbe31305ecc06e20f757d2f6
okikechinonso <chinonsookike@gmail.com>
2022-09-19 11:11:07 +0100

    new commit

--- chatcrib/src/components/scrumboard/scrumboard.jsx
+++ chatcrib/src/components/scrumboard/scrumboard.jsx
@@ -5,33 +5,53 @@ import './scrumboard.css'
 
 
 export class Scrumboard extends Component {
-    state = details
+  state = details
   render() {
     return (
       <div>
-         <nav className='nav bg-primary'>
+        <nav className='nav bg-primary'>
           <h3>CHATSCRUM</h3>
           <div >
-              <p>User Type: {details.userType}</p>
-              <p>Project Name: {details.projectName}</p>
+            <p>User Type: {details.userType}</p>
+            <p>Project Name: {details.projectName}</p>
           </div>
-      </nav>
-      <h4>Hello {details.fullname} Welcome to your scrumboard</h4>
-      <div>
-        <div style={{display: "flex"}}>
+        </nav>
+        <h4>Hello {details.fullname} Welcome to your scrumboard</h4>
         <div className='task-container'>
-          <h3 className='bg-primary'>Weekly Tasks</h3>
+          <div style={{ display: "flex" }}>
+            <div>
+            <div className='task-content'>
+              <h3 className='bg-primary'>Weekly Tasks</h3>
+            </div>
+            <button class='btn btn-primary'>ADD TASK</button>
+            </div>
+            
+            <div className='task-container'>
+              <div className='task-content'>
+                <h3 className='bg-primary'>Daily Target</h3>
+              </div>
+              <button class='btn btn-primary'>ADD TASK</button>
+            </div>
+
+          </div>
+          <div class="modal" >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Modal title</h5>
+                  <button type="button" class="btn-close"  aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-        <div className='task-container'>
-          <h3 className='bg-primary'>Daily Target</h3>
-        </div>
-        </div>
-        
-        </div>
-        
-        <button class='btn btn-primary'>ADD TASK</button>
-      </div>
       </div>
     )
   }
