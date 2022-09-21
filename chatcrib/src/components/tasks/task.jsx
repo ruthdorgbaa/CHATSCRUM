6df3a1c49ce0f541aa25d64625688ca3c7921114
okikechinonso <chinonsookike@gmail.com>
2022-09-21 10:12:54 +0100

    new commit

--- chatcrib/src/components/tasks/task.jsx
+++ chatcrib/src/components/tasks/task.jsx
@@ -1,13 +1,12 @@
 import React, {useState} from 'react'
 import {tasklist} from '../../static/task'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
-import { useState } from 'react'
 
 export default function Tasks() {
-    const [taskRoll, updateTaskRoll] = useState(taskList)
+    const [taskRoll, updateTaskRoll] = useState(tasklist)
 
     const handleOnDragEnd = result => {
-        const item = Array.from(taskRoll);
+        const items = Array.from(taskRoll);
         const [reorderedItem] = items.splice(result.source.index, 1);
         items.splice(result.destination.index, 0, reorderedItem);
 
@@ -20,42 +19,33 @@ export default function Tasks() {
                 <div className="container">
                     <Droppable droppableId="tasket">
                         {(provided) => (
-                            <div className="weekly box" {...provided.droppleProps} ref={...provided.innerRef}>
+                            <div className="weekly box" {...provided.droppableProps} ref={provided.innerRef}>
                                 <h3>Weekly Tasks</h3>
               
-                                {tasklist.map(({ id, item }) =>
+                                {tasklist.map(({ id, item },index) =>
                                 
                                     <Draggable key={id} draggableId={id} index={index}>
                                         {(provided) => (
-                                            <p className="task" {...provided.draggableProps} {...provided.dragHandleProps} ref={...provided}>
+                                            <p className="task" {...provided.droppableProps} {...provided.dragHandleProps} ref={provided}>
                                                 {item}
                                             </p>
                                         )}
                                         {provided.placeholder}
                                     </Draggable>
                       
-                                )
-                                  
-                          
-                                })}
+                                )}
                             </div>
                         )}
                     </Droppable>
                     <Droppable id="tasker">
                         {(provided) => (
-                            div className="daily box" {...provided.droppleProps} ref={provided.innerRef}>
-                        <h3>Daily Target</h3>
+                            <div className="daily box" {...provided.droppableProps} ref={provided.innerRef}>
+                                <h3>Daily Target</h3>
+                            </div> 
+                        )}
+                    </Droppable> 
                 </div>
-                {provided.placeholder}
-                )
-            </Droppable>
-                  
-        </div>
-      
-      </div >
-        
-       
-                   </DragDropContext >      
-  )
-
+            </DragDropContext >            
+       </div >                
+    )
 }
